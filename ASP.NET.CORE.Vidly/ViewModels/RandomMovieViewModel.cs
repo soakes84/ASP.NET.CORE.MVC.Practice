41d0e12acef6e20995e9e6184e8d39ce14e28dae
Spencer Oakes <spencer.oakes84@gmail.com>
2017-11-09 14:17:09 -0500

    Added NavBar Partial View

--- ASP.NET.CORE.Vidly/ViewModels/RandomMovieViewModel.cs
+++ ASP.NET.CORE.Vidly/ViewModels/RandomMovieViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using ASP.NET.CORE.Vidly.Models;
 namespace ASP.NET.CORE.Vidly.ViewModels
 {
     public class RandomMovieViewModel
     {
-        public RandomMovieViewModel()
-        {
-        }
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
     }
 }
